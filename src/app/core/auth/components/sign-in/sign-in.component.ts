b30054667f30850c6d0c38dff03094f0dc11668b
Ali PARLATTI <aliparlatti@gmail.com>
2025-02-07 17:21:57 +0300

    feature: signIn

--- src/app/core/auth/components/sign-in/sign-in.component.ts
+++ src/app/core/auth/components/sign-in/sign-in.component.ts
@@ -1,14 +1,50 @@
-import {Component} from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnDestroy} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgClass} from '@angular/common';
+import {AuthService} from '../../auth.service';
+import {Subject, takeUntil} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    NgClass,
+    ReactiveFormsModule
+  ],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
+export class SignInComponent implements OnDestroy {
+  _unsubscribeAll: Subject<any> = new Subject<any>();
+  loginForm: FormGroup;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,private fb: FormBuilder, private authService: AuthService,) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService
+        .signIn(this.loginForm.value)
+        .pipe(
+          takeUntil(this._unsubscribeAll),
+        )
+        .subscribe({
+          next: (value) => {
+            if (value) {
+              this.router.navigate(['home']);
+            }
+          },
+        });
+    } else {
+      return
+    }
+  }
+  ngOnDestroy() {
+    this._unsubscribeAll.next(true);
+    this._unsubscribeAll.complete();
   }
 }
--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,12 +1,23 @@
 import {Component } from '@angular/core';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: 'layout.component.html',
-  imports: [
-  ],
+  imports:[],
+  styles:`
+    .navbar-toggler:focus,
+    .navbar-toggler:active,
+    .navbar-toggler-icon:focus {
+      outline: none;
+      box-shadow: none;
+    }
+    .user-name{
+     font-size: 1rem;
+    }
+  `
 })
 export class LayoutComponent {
-  constructor() {
+  constructor(protected authService: AuthService) {
   }
 }
